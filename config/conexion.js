b5d3b72d957a21775336531cd36c892030f652c3
Aaron-Imba <aiimbaquingof@utn.edu.ec>
2025-01-06 19:59:15 -0500

    Update conexion.js

--- config/conexion.js
+++ config/conexion.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize('gmte', 'postgres', '1234', {
+const sequelize = new Sequelize('gmte', 'postgres', 'aaron', {
     host: 'localhost',
     dialect: 'postgres',
     port: 5432,
