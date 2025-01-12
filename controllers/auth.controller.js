7917e1ce7ed661600e561e5aa56f19a2b5a463c9
Jesus Tandayamo <jesustandayamo255@gmail.com>
2025-01-11 23:48:11 -0500

    creacion del jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,6 @@
+import { createToken } from "../libs/jwt.js";
 import Usuario from "../model/usuario.model.js";
+
 
 export const login = async (req, res) => {
     const {user_email, user_password} = req.body;
@@ -11,7 +13,11 @@ export const login = async (req, res) => {
 
         if (!userFound) return res.status(404).json({ message: "User not found" });
 
-        res.json({message: "User found"});
+        if (userFound.user_password !== user_password) return res.status(401).json({ message: "Invalid password" });
+
+        const token = await createToken({id: userFound.user_id});
+        res.cookie("token", token);
+        res.json({message: "Logeado", userFound});
     } catch (error) {
         res.status(500).json({
             message: error.message || "Some error occurred while retrieving users."
