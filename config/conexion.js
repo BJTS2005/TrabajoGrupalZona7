8464047503a68c98e28ed3b7f3f131b38576067d
Jesus Tandayamo <jesustandayamo255@gmail.com>
2025-01-20 21:42:26 -0500

    Correguida la base de datos y actualizado los controladoress

--- config/conexion.js
+++ config/conexion.js
@@ -1,15 +1,21 @@
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize('gmte', 'postgres', '1234', {
-    host: 'localhost',
+const sequelize = new Sequelize('postgres', 'GM_TE', 'GM_TE', {
+    host: 'bdd-unificada.postgres.database.azure.com',
     dialect: 'postgres',
     port: 5432,
+    dialectOptions: {
+        ssl: {
+            require: true, 
+            rejectUnauthorized: false, 
+        },
+    },
 });
 
 export const testDbConnection = async () => {
     try {
-        await sequelize.authenticate();
+        await sequelize.authenticatae();
         console.log('Conexión exitosa a la base de datos');
     } catch (error) {
         console.log('Error al conectar la base de datos', error);
